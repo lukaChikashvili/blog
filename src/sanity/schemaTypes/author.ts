fe26d618695622b7926025aa34f4d66372ac568d
luka chikashvili <lukachikashvili2020@gmail.com>
2025-03-11 01:31:28 +0400

    add author schema document in sanity

---
+++ src/sanity/schemaTypes/author.ts
@@ -0,0 +1,54 @@
+import { defineField, defineType } from "sanity";
+
+export const author = defineType({
+    name: "author",
+    title: "author",
+    type: "document",
+    fields: [
+        defineField({
+            name: "id",
+            type: "number",
+
+        }),
+
+        defineField({
+            name: "name",
+            type: "string",
+
+        }),
+
+        defineField({
+            name: "username",
+            type: "string",
+
+        }),
+
+        defineField({
+            name: "email",
+            type: "string",
+
+        }),
+
+        defineField({
+            name: "image",
+            type: "url",
+
+        }),
+
+        defineField({
+            name: "bio",
+            type: "text",
+
+        }),
+
+
+    ],
+
+    preview: {
+        select: {
+            title: "name"
+        }
+    }
+
+
+})
