4b8cadfbb55f25dc15071bdf01ef79b0b9c80730
luka chikashvili <lukachikashvili2020@gmail.com>
2025-03-14 19:52:13 +0400

    add hero section text

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import { client } from "@/sanity/lib/client";
 import { BLOGS_QUERY } from "@/sanity/lib/queries";
 import Image from "next/image";
+import Hero from "../components/Hero";
 
 export default async function Home() {
 
@@ -10,23 +11,8 @@ export default async function Home() {
 
   return (
    <>
-    <div>
-      {posts.map((value:any) => (
-        <div key={value._id}>
-           <h1>{value.title}</h1>
-           <div>
-            
-           <h1> {value.author.name}</h1>
-           <p>{value.category}</p>
-           <p>{value.description}</p>
+     <Hero />
 
-
-            </div>
-          
-           
-          </div>
-      ))}
-    </div>
    </>
   );
 }
---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='text-white w-full px-50 py-12 '>
+      
+
+    <h1 className='text-6xl text-center w-[80%] m-auto leading-tight '>
+        Exploring the <span className = "text-green-400">Mind,</span> Code, and Language <span className='text-green-400'>—</span> One Post at a Time</h1>
+    </div>
+  )
+}
+
+export default Hero
