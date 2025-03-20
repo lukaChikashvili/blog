7425a5535155b6a7672f67652872389053d8ed47
luka chikashvili <lukachikashvili2020@gmail.com>
2025-03-20 16:21:25 +0400

    change image host

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "media.licdn.com", 
       },
+      {
+        protocol: "https",
+        hostname: "polyglotclub.com", 
+      },
     ],
   },
 };
